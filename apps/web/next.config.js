46a6d75642bd12cf538f9d074366a627864dc6cd
Tosin Shada <89518803+TosinShada@users.noreply.github.com>
2024-11-19 10:16:38 +0100

    refactor: Remove Umami Analytics integration from layout, middleware, and config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/umami/script.js',
-        destination: `https://eu.umami.is/script.js`,
-      },
-      {
-        source: '/umami/api/send',
-        destination: `https://eu.umami.is/api/send`,
-      },
-    ]
-  },
   reactStrictMode: false,
   output: 'standalone',
 }
